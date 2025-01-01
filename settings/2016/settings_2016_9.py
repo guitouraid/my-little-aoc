f31a7c4f6ec29b70fd5ae746e441ff7b957a6fa1
guitouraid <guitouraid@gmail.com>
2025-01-01 20:29:55 +0100

    more efficient solution for 2016-9

--- settings/2016/settings_2016_9.py
+++ settings/2016/settings_2016_9.py
@@ -15,25 +15,27 @@ TEST_DATA_2 = """
 ### store file under `DATA_DIR`
 REAL_FILE_2 = REAL_FILE_1 = "2016_9.txt"
 
-RE_REPEAT = re.compile(r'\((\d+)x(\d+)\)')
+RE_REPEAT = re.compile(r'(.*?)\((\d+)x(\d+)\)(.*)')
 
-def decoded_len(input: str, recurse=False) -> int:
-    if m := RE_REPEAT.search(input):
-        length, mult = [int(x) for x in m.groups()]
-        istart = m.start()
-        tstart = m.end()
-        tend = tstart+length
+def decoded_len(input: str, recurse=False):
+    if m := RE_REPEAT.match(input):
+        before, length, mult, after = m.groups()
+        length = int(length)
+        mult = int(mult)
+        yield len(before)
         if recurse:
-            return len(input[:istart]) + decoded_len(input[tstart:tend]*mult, recurse) + decoded_len(input[tend:], recurse)
+            yield from map(lambda x: x*mult,decoded_len(after[:length], recurse))
         else:
-            return len(input[:istart]) + len(input[tstart:tend]*mult) + decoded_len(input[tend:])
-    return len(input)
+            yield len(after[:length]*mult)
+        yield from decoded_len(after[length:], recurse)
+    else:
+        yield len(input)
 
 def exo1(data: str) -> int:
-    return decoded_len(data)
+    return sum(decoded_len(data))
 
 def exo2(data: str) -> int:
-    return decoded_len(data, True)
+    return sum(decoded_len(data, True))
     
 
 settings = (
