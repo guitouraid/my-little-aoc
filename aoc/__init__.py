bf838f4c3540727c6b7347388a416bf5f06395cb
guitouraid <guitouraid@gmail.com>
2025-07-06 16:37:21 +0200

    add nostrip read mode

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -3,13 +3,14 @@ from enum import Enum
 class ReadMode(str, Enum):
     READ_LINES = "lines"
     READ_ALL = "all"
+    READ_NOSTRIP = "nostrip"
 
     @property
     def data_type(self):
         match self:
             case ReadMode.READ_LINES:
                 return 'list[str]'
-            case ReadMode.READ_ALL:
+            case ReadMode.READ_ALL|ReadMode.READ_NOSTRIP:
                 return 'str'
 
 class InputType(str, Enum):
